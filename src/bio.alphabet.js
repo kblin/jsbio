3a9ccfe0b60165a041d21301e025428f7d904840
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2013-05-07 16:27:03 +0200

    Bio.Alphabet: Add IUPAC Protein/DNA/RNA alphabets
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- src/bio.alphabet.js
+++ src/bio.alphabet.js
@@ -14,8 +14,62 @@ Bio.SingleLetterAlphabet = Bio.Alphabet.extend({
     }
 });
 
-Bio.ProteinAlphabet = Bio.SingleLetterAlphabet.extend({
+Bio.ProteinAlphabet = Bio.SingleLetterAlphabet.extend({});
+Bio.generic_protein = new Bio.ProteinAlphabet();
+
+Bio.NucleotideAlphabet = Bio.SingleLetterAlphabet.extend({});
+Bio.generic_nucleotide = new Bio.NucleotideAlphabet();
+
+Bio.DNAAlphabet = Bio.NucleotideAlphabet.extend({});
+Bio.generic_dna = new Bio.DNAAlphabet();
+
+Bio.RNAAlphabet = Bio.NucleotideAlphabet.extend({});
+Bio.generic_rna = new Bio.DNAAlphabet();
+
+Bio.IUPACProtein = Bio.ProteinAlphabet.extend({
     init: function() {
         this._super();
+        this.letters = Bio.Data.IUPACData.protein_letters;
     }
 });
+Bio.protein = new Bio.IUPACProtein();
+
+Bio.ExtendedIUPACProtein = Bio.ProteinAlphabet.extend({
+    init: function() {
+        this._super();
+        this.letters = Bio.Data.IUPACData.extended_protein_letters;
+    }
+});
+Bio.extended_protein = new Bio.ExtendedIUPACProtein();
+
+Bio.IUPACUnambiguousDNA = Bio.DNAAlphabet.extend({
+    init: function() {
+        this._super();
+        this.letters = Bio.Data.IUPACData.unambiguous_dna_letters;
+    }
+});
+Bio.unambiguous_dna = new Bio.IUPACUnambiguousDNA();
+
+Bio.IUPACAmbiguousDNA = Bio.DNAAlphabet.extend({
+    init: function() {
+        this._super();
+        this.letters = Bio.Data.IUPACData.ambiguous_dna_letters;
+    }
+});
+Bio.ambiguous_dna = new Bio.IUPACAmbiguousDNA();
+
+Bio.IUPACUnambiguousRNA = Bio.RNAAlphabet.extend({
+    init: function() {
+        this._super();
+        this.letters = Bio.Data.IUPACData.unambiguous_rna_letters;
+    }
+});
+Bio.unambiguous_rna = new Bio.IUPACUnambiguousRNA();
+
+Bio.IUPACAmbiguousRNA = Bio.RNAAlphabet.extend({
+    init: function() {
+        this._super();
+        this.letters = Bio.Data.IUPACData.ambiguous_rna_letters;
+    }
+});
+Bio.ambiguous_rna = new Bio.IUPACAmbiguousRNA();
--- tests/test_bio_alphabet.js
+++ tests/test_bio_alphabet.js
@@ -46,3 +46,156 @@ test("ProteinAlphabet instantiation", function(){
     ok(a instanceof Bio.SingleLetterAlphabet);
     ok(a instanceof Bio.Alphabet);
 });
+
+test("Bio.generic_protein is instance of ProteinAlphabet", function(){
+    ok(Bio.generic_protein instanceof Bio.ProteinAlphabet);
+});
+
+test("NucleotideAlphabet instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.NucleotideAlphabet();
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.generic_nucleotide is instance of NucleotideAlphabet", function(){
+    ok(Bio.generic_nucleotide instanceof Bio.NucleotideAlphabet);
+});
+
+test("DNAAlphabet instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.DNAAlphabet();
+    ok(a instanceof Bio.DNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.generic_dna is instance of DNAAlphabet", function(){
+    ok(Bio.generic_dna instanceof Bio.DNAAlphabet);
+});
+
+test("RNAAlphabet instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.RNAAlphabet();
+    ok(a instanceof Bio.RNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.generic_rna is instance of RNAAlphabet", function(){
+    ok(Bio.generic_rna instanceof Bio.DNAAlphabet);
+});
+
+test("IUPACProtein instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.IUPACProtein();
+    ok(a instanceof Bio.IUPACProtein);
+    ok(a instanceof Bio.ProteinAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.protein is instance of IUPACProtein", function(){
+    ok(Bio.protein instanceof Bio.IUPACProtein);
+});
+
+test("ExtendedIUPACProtein instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.ExtendedIUPACProtein();
+    ok(a instanceof Bio.ExtendedIUPACProtein);
+    ok(a instanceof Bio.ProteinAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.protein is instance of ExtendedIUPACProtein", function(){
+    ok(Bio.extended_protein instanceof Bio.ExtendedIUPACProtein);
+});
+
+test("IUPACUnambiguousDNA instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.IUPACUnambiguousDNA();
+    ok(a instanceof Bio.IUPACUnambiguousDNA);
+    ok(a instanceof Bio.DNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.IUPACUnambiguousDNA uses correct alphabet", function(assert) {
+    var a = new Bio.IUPACUnambiguousDNA();
+    assert.equal(a.letters, Bio.Data.IUPACData.unambiguous_dna_letters);
+});
+
+test("Bio.unambiguous_dna is instance of IUPACUnambiguousDNA", function(){
+    ok(Bio.unambiguous_dna instanceof Bio.IUPACUnambiguousDNA);
+});
+
+test("IUPACAmbiguousDNA instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.IUPACAmbiguousDNA();
+    ok(a instanceof Bio.IUPACAmbiguousDNA);
+    ok(a instanceof Bio.DNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.IUPACAmbiguousDNA uses correct alphabet", function(assert) {
+    var a = new Bio.IUPACAmbiguousDNA();
+    assert.equal(a.letters, Bio.Data.IUPACData.ambiguous_dna_letters);
+});
+
+test("Bio.ambiguous_dna is instance of IUPACAmbiguousDNA", function(){
+    ok(Bio.ambiguous_dna instanceof Bio.IUPACAmbiguousDNA);
+});
+
+test("IUPACUnambiguousRNA instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.IUPACUnambiguousRNA();
+    ok(a instanceof Bio.IUPACUnambiguousRNA);
+    ok(a instanceof Bio.RNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.IUPACUnambiguousRNA uses correct alphabet", function(assert) {
+    var a = new Bio.IUPACUnambiguousRNA();
+    assert.equal(a.letters, Bio.Data.IUPACData.unambiguous_rna_letters);
+});
+
+test("Bio.unambiguous_rna is instance of IUPACUnambiguousRNA", function(){
+    ok(Bio.unambiguous_rna instanceof Bio.IUPACUnambiguousRNA);
+});
+
+test("IUPACAmbiguousRNA instantiation", function(){
+    var a = null;
+    ok(a === null);
+    a = new Bio.IUPACAmbiguousRNA();
+    ok(a instanceof Bio.IUPACAmbiguousRNA);
+    ok(a instanceof Bio.RNAAlphabet);
+    ok(a instanceof Bio.NucleotideAlphabet);
+    ok(a instanceof Bio.SingleLetterAlphabet);
+    ok(a instanceof Bio.Alphabet);
+});
+
+test("Bio.IUPACAmbiguousRNA uses correct alphabet", function(assert) {
+    var a = new Bio.IUPACAmbiguousRNA();
+    assert.equal(a.letters, Bio.Data.IUPACData.ambiguous_rna_letters);
+});
+
+test("Bio.ambiguous_rna is instance of IUPACAmbiguousRNA", function(){
+    ok(Bio.ambiguous_rna instanceof Bio.IUPACAmbiguousRNA);
+});
